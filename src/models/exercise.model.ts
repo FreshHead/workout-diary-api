1b84a58d0113b4e82756a241204e75e5c3e26397
Alexander Kostarev <UniverAlex@ya.ru>
2026-04-23 20:27:20 +0300

    exercise model

---
+++ src/models/exercise.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Exercise extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  sets: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  reps: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  weight: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  workoutId: string;
+
+
+  constructor(data?: Partial<Exercise>) {
+    super(data);
+  }
+}
+
+export interface ExerciseRelations {
+  // describe navigational properties here
+}
+
+export type ExerciseWithRelations = Exercise & ExerciseRelations;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
 export * from './workout.model';
+export * from './exercise.model';
